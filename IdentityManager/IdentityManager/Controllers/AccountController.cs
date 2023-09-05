524067764512501757ba757367646aed4f0fd5f4
David Riggins <driggins@esri.com>
2023-09-05 10:52:26 -0700

    Lesson 38 End. Confirm Email.

--- IdentityManager/IdentityManager/Controllers/AccountController.cs
+++ IdentityManager/IdentityManager/Controllers/AccountController.cs
@@ -73,6 +73,26 @@ namespace IdentityManager.Controllers
 
 
         [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId==null || code==null)
+            {
+                return View("Error");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return View("Error");
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+
+            return View(result.Succeeded ? "ConfirmEmail" : "Error");
+        }
+
+
+        [HttpGet]
         public IActionResult Login(string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
