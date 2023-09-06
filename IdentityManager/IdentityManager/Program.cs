d0b4b8e8dc7a64f6a0dc99e60e864fa81b610804
David Riggins <driggins@esri.com>
2023-09-06 14:17:35 -0700

    Lesson 63 End. Scaffold Identity Class Library.

--- IdentityManager/IdentityManager/Program.cs
+++ IdentityManager/IdentityManager/Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
 // Add Identity. Relation between users, roles and stores
 builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
