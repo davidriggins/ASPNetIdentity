95bb8c0aa819ef6c978711c9d75b66514e6afc12
David Riggins <driggins@esri.com>
2023-09-06 13:53:46 -0700

    Lesson 59 End. Role Demo.

--- IdentityManager/IdentityManager/Controllers/HomeController.cs
+++ IdentityManager/IdentityManager/Controllers/HomeController.cs
@@ -33,7 +33,7 @@ namespace IdentityManager.Controllers
 
 
         // Means only an authorized user can access this method
-        [Authorize]
+        [Authorize(Roles = "Admin")]
         public IActionResult Privacy()
         {
             return View();
