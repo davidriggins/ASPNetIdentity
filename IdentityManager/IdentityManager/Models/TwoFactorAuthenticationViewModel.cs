097ed79cf84ba87fa81e087e06748cba7cd3260e
David Riggins <driggins@esri.com>
2023-09-06 09:04:51 -0700

    Lesson 45 End. QR Code JS and Enable Authenticator Get Method.

---
+++ IdentityManager/IdentityManager/Models/TwoFactorAuthenticationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentityManager.Models
+{
+    public class TwoFactorAuthenticationViewModel
+    {
+        // Used to login
+        public string Code { get; set; }
+
+        // Used to Register/Signup
+        public string Token { get; set; }
+    }
+}
