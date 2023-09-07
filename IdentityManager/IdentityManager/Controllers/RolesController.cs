f4631df162b07cd23ac4a89003ca612226c19b21
David Riggins <driggins@esri.com>
2023-09-06 17:53:34 -0700

    Lesson 67 End. Roles List Action Method.

---
+++ IdentityManager/IdentityManager/Controllers/RolesController.cs
@@ -0,0 +1,28 @@
+﻿using IdentityManager.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(ApplicationDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+
+
+            return View(roles);
+        }
+    }
+}
